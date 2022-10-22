6a373fc3df070b526466af4dd4422f200f0878a6
Ahmed Shukri <ahmed.s100402@gmail.com>
2022-10-22 23:17:20 +0100

    Fixed issues with duplicate stock pricing and graph plotting

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean
 }
 
 /**
@@ -22,6 +23,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false
     };
   }
 
@@ -36,11 +38,13 @@ class App extends Component<{}, IState> {
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        // Update the state by creating a new array of data that consists of
+        // Previous data in the state and the new data from server
+        this.setState({ data: [...this.state.data, ...serverResponds], showGraph: true });
+      });
+    }, 100)
   }
 
   /**
@@ -63,7 +67,7 @@ class App extends Component<{}, IState> {
             Start Streaming Data
           </button>
           <div className="Graph">
-            {this.renderGraph()}
+            {this.state.showGraph && this.renderGraph()}
           </div>
         </div>
       </div>
